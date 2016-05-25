99814d2487b1d6a83cb6fc68dabbe3c92e0a5f83
dyoon <dyoon@node-3.dyoon-qv16129.rdmaresearch-pg0.clemson.cloudlab.us>
2016-05-25 17:47:56 -0400

    added rdma/infiniband setup script for ubuntu

---
+++ ubuntu_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get -y --force-yes install libibcm1 libibverbs1 ibverbs-utils librdmacm1 rdmacm-utils ibsim-utils ibutils libcxgb3-1 libibmad5 libibumad3 libmlx4-1 libmthca1 libnes1 rds-tools infiniband-diags libibcommon1 mstflint opensm libopensm5 perftest srptools
+sudo apt-get install librdmacm-dev libibverbs-dev libipathverbs1
